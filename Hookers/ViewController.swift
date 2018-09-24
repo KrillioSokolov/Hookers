0ee4498f619ea41481726683a43dc3bf1c5b64cb
Kirill <>
2018-09-24 20:00:40 +0300

    first commit

---
+++ Hookers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hookers
+//
+//  Created by Kirill Sokolov on 24.09.2018.
+//  Copyright © 2018 Kirill Sokolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
